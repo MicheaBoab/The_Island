fd671fcbac56a4d2efdd38f029a63d6eaed2f71f
Parth4337 <parth.sanghani@yahoo.com>
2020-11-09 13:33:43 -0600

    remoed imported trees and added custom trees
    Removed imported trees as well. Created tree using unity tree utility, hopefully will have better performance

---
+++ Assets/Custom/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //public static GameManager instance;
+
+    public PlayerController player;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //if (instance == null) instance = this;
+        //else if (instance != this) Destroy(gameObject);
+        //DontDestroyOnLoad(gameObject);
+        Debug.Log("Setting transform in GM");
+        Debug.Log(SaveData.current.playerPosition);
+        player.LoadData(SaveData.current.playerPosition, SaveData.current.playerRotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
